16451e80e7a4f3188eac12b7b0f188a64c1bbad8
anhchisg99 <anhchictw311@gmaiil.com>
2024-02-20 23:24:33 +0700

    add useEffect, consume API

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,24 @@
 import Button from './Button.js'
+import { useState, useEffect } from 'react'
+
 
 export default function Content() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data)
+                setUsers(data)
+            })
+            .catch((err)=> console.log(err.message))
+    })
+    const userComp= users.map(user=>(
+        <li keu={user.id}>
+            {user.title}
+        </li>
+    ))
     return (
         <div className="content">
             <h1>this is content</h1>
@@ -8,7 +26,12 @@ export default function Content() {
                 voluptatem, incidunt adipisci expedita unde blanditiis voluptate quisquam dolorum numquam in deserunt
                 laborum, doloremque similique nisi.
             </p>
-            <Button/>
+            <div className="user-list">
+                <ul>
+                    {userComp}
+                </ul>
+            </div>
+            <Button />
         </div>
     )
 }
